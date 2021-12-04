e8b966eb1754859dc7aaa1903f70952f1782bdaf
= <vladimir.inves@gmail.com>
2021-12-04 14:22:27 +0300

    append time

--- starbucks_parse.py
+++ starbucks_parse.py
@@ -1,38 +1,42 @@
+import time
+
 import openpyxl
 from geopy.geocoders import ArcGIS
 import json
+import time
 
 def get_data():
     try:
         book = openpyxl.open("starbucks.xlsx", read_only=True)
+
+        sheet = book.active
+        for i in range(130, sheet.max_row+1):
+            adress = sheet[i][1].value  # [row][column]
+            name = sheet[i][0].value
+            print(f"Сохраняю место с именем {name} {i}/{sheet.max_row}")
+            try:
+                response = ArcGIS(user_agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36').geocode(adress)
+                latitude = response.latitude  # широта
+                longitude = response.longitude  # долгота
+            except:
+                latitude = 'Адрес не найден'
+                longitude = 'Адрес не найден'
+            adress_part = adress.replace(',', '').replace('.', ' ').split()
+            try:
+                city = list(set(adress_part) & set(cities))[0]
+            except Exception as ex:
+                print(ex)
+                city = 'Город не найден'
+            places = []
+            places.append({
+                "coords": [latitude, longitude],
+                "name": name,
+                "adress": adress
+            })
+            write_json(city, places)
     except Exception as ex:
         print(ex)
         print('Файл starbucks.xlsx не найден')
-    sheet = book.active
-    for i in range(2, sheet.max_row+1):
-        adress = sheet[i][1].value  # [row][column]
-        name = sheet[i][0].value
-        print(f"Сохраняю место с именем {name} {i}/{sheet.max_row}")
-        try:
-            response = ArcGIS(user_agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36').geocode(adress)
-            latitude = response.latitude  # широта
-            longitude = response.longitude  # долгота
-        except:
-            latitude = 'Адрес не найден'
-            longitude = 'Адрес не найден'
-        adress_part = adress.replace(',', '').replace('.', ' ').split()
-        try:
-            city = list(set(adress_part) & set(cities))[0]
-        except Exception as ex:
-            print(ex)
-            city = 'Город не найден'
-        places = []
-        places.append({
-            "coords": [latitude, longitude],
-            "name": name,
-            "adress": adress
-        })
-        write_json(city, places)
 
 cities = ['Москва', 'Московская область', 'Санкт-Петербург', 'Казань',
     'Самара', 'Екатеринбург', 'Тюмень',
@@ -55,6 +59,7 @@ def write_json(city, places):
 
 def main():
     get_data()
+    time.sleep(5)
 
 
 if __name__== "__main__":
