73f89a05f01cbe61764c3f90798a452ad0ca05c6
= <vladimir.inves@gmail.com>
2021-12-04 13:48:24 +0300

    add folder and correct code

--- starbucks_parse.py
+++ starbucks_parse.py
@@ -3,24 +3,19 @@ from geopy.geocoders import ArcGIS
 import json
 
 def get_data():
-
     book = openpyxl.open("starbucks.xlsx", read_only=True)
     sheet = book.active
-    for i in range(130,sheet.max_row+1): #sheet.max_row+1
-        #adress = sheet[i][1].value +','+ sheet[i][2].value #[row][column]
+    for i in range(2,sheet.max_row+1):
         adress = sheet[i][1].value  # [row][column]
         name = sheet[i][0].value
-
         print(f"Сохраняю место с именем {name} {i}/{sheet.max_row}")
         try:
-            #response = Nominatim(user_agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36').geocode(adress)
             response = ArcGIS(user_agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36').geocode(adress)
             latitude = response.latitude  # широта
             longitude = response.longitude  # долгота
         except:
             latitude = 'Адрес не найден'
             longitude = 'Адрес не найден'
-
         adress_part = adress.replace(',', '').replace('.', ' ').split()
         try:
             city = list(set(adress_part) & set(cities))[0]
@@ -49,14 +44,13 @@ cities_json = {
 
 # Сохраняем в формат JSON и открываем файл
 def write_json(city, places):
-    with open(cities_json[city], "a", encoding="utf-8") as file:
+    with open('cities/'+cities_json[city], "a", encoding="utf-8") as file:
         json.dump(places, file, indent=4, ensure_ascii=False)
-        #file.write('\n')
 
 
 def main():
     get_data()
-    #write_json()
+
 
 if __name__== "__main__":
     main()
