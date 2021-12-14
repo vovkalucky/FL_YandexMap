19c80490b01b8a0e97c8470080068f9324d246c7
= <vladimir.inves@gmail.com>
2021-12-14 23:11:56 +0300

    Fix output json

--- starbucks_parse.py
+++ starbucks_parse.py
@@ -10,17 +10,18 @@ def get_data():
         book = openpyxl.open("starbucks.xlsx", read_only=True)
 
         sheet = book.active
-        for i in range(2, sheet.max_row+1):
+        for i in range(2, sheet.max_row+1): #sheet.max_row+1
             adress = sheet[i][1].value  # [row][column]
             name = sheet[i][0].value
             print(f"Сохраняю место с именем {name} {i}/{sheet.max_row}")
             try:
                 response = ArcGIS(user_agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36').geocode(adress)
-                latitude = response.latitude  # широта
-                longitude = response.longitude  # долгота
+                latitude = round(response.latitude,3)  # широта
+                longitude = round(response.longitude,3)  # долгота
             except:
                 latitude = 'Адрес не найден'
                 longitude = 'Адрес не найден'
+
             adress_part = adress.replace(',', '').replace('.', ' ').split()
             try:
                 city = list(set(adress_part) & set(cities))[0]
@@ -52,8 +53,8 @@ cities_json = {
 # Сохраняем в формат JSON и открываем файл
 def write_json(city, places):
     with open('cities/'+cities_json[city]+'.json', "a", encoding="utf-8") as file:
-        json.dump(places, file, indent=4, ensure_ascii=False)
-        file.write(';\n')
+        json.dump(places, file, ensure_ascii=False)
+        file.write(',\n')
 
 
 def main():
